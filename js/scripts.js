a46c18e846933ffff1b9d2a9a2f671a0aa21b1d7
hyphenate93 <50260177+hyphenate93@users.noreply.github.com>
2024-03-10 18:47:24 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -215,10 +215,7 @@ $('#rsvp-form').on('submit', function (e) {
 
     $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-    if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-        && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-        $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-    } else {
+
         $.post('https://script.google.com/macros/s/AKfycbzoAH2GzGJG0sldaQX-zwzziGwqpj4LuabjX2bB9J25UhVNG5dtFXgxOJwE4hEO2QRvTQ/exec', data)
             .done(function (data) {
                 console.log(data);
@@ -233,7 +230,7 @@ $('#rsvp-form').on('submit', function (e) {
                 console.log(data);
                 $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
             });
-    }
+    
 });
 
 
